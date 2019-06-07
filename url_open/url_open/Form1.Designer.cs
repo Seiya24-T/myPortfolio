a898deb956e7a406dc6ed5b80dfe9dd2181d4f8f
Seiya24-T <sas.sp58@gmail.com>
2019-06-07 17:12:22 +0900

    20190607

---
+++ url_open/url_open/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace url_open
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb_url = new System.Windows.Forms.TextBox();
+            this.lb_url = new System.Windows.Forms.ListBox();
+            this.bt_insert = new System.Windows.Forms.Button();
+            this.bt_open = new System.Windows.Forms.Button();
+            this.bt_delete = new System.Windows.Forms.Button();
+            this.bt_allopen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tb_url
+            // 
+            this.tb_url.Font = new System.Drawing.Font("MS UI Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.tb_url.Location = new System.Drawing.Point(12, 12);
+            this.tb_url.Name = "tb_url";
+            this.tb_url.Size = new System.Drawing.Size(324, 35);
+            this.tb_url.TabIndex = 0;
+            // 
+            // lb_url
+            // 
+            this.lb_url.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lb_url.FormattingEnabled = true;
+            this.lb_url.ItemHeight = 24;
+            this.lb_url.Location = new System.Drawing.Point(12, 55);
+            this.lb_url.Name = "lb_url";
+            this.lb_url.Size = new System.Drawing.Size(324, 148);
+            this.lb_url.TabIndex = 2;
+            this.lb_url.SelectedIndexChanged += new System.EventHandler(this.Lb_url_SelectedIndexChanged);
+            // 
+            // bt_insert
+            // 
+            this.bt_insert.Font = new System.Drawing.Font("MS UI Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.bt_insert.Location = new System.Drawing.Point(351, 12);
+            this.bt_insert.Name = "bt_insert";
+            this.bt_insert.Size = new System.Drawing.Size(170, 90);
+            this.bt_insert.TabIndex = 1;
+            this.bt_insert.Text = "Insert";
+            this.bt_insert.UseVisualStyleBackColor = true;
+            this.bt_insert.Click += new System.EventHandler(this.Bt_insert_Click);
+            // 
+            // bt_open
+            // 
+            this.bt_open.Font = new System.Drawing.Font("MS UI Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.bt_open.Location = new System.Drawing.Point(351, 116);
+            this.bt_open.Name = "bt_open";
+            this.bt_open.Size = new System.Drawing.Size(170, 90);
+            this.bt_open.TabIndex = 3;
+            this.bt_open.Text = "Open";
+            this.bt_open.UseVisualStyleBackColor = true;
+            this.bt_open.Click += new System.EventHandler(this.Bt_open_Click);
+            // 
+            // bt_delete
+            // 
+            this.bt_delete.Font = new System.Drawing.Font("MS UI Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.bt_delete.Location = new System.Drawing.Point(527, 12);
+            this.bt_delete.Name = "bt_delete";
+            this.bt_delete.Size = new System.Drawing.Size(170, 90);
+            this.bt_delete.TabIndex = 4;
+            this.bt_delete.Text = "Delete";
+            this.bt_delete.UseVisualStyleBackColor = true;
+            this.bt_delete.Click += new System.EventHandler(this.Bt_delete_Click);
+            // 
+            // bt_allopen
+            // 
+            this.bt_allopen.Font = new System.Drawing.Font("MS UI Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.bt_allopen.Location = new System.Drawing.Point(527, 116);
+            this.bt_allopen.Name = "bt_allopen";
+            this.bt_allopen.Size = new System.Drawing.Size(170, 90);
+            this.bt_allopen.TabIndex = 5;
+            this.bt_allopen.Text = "All Open";
+            this.bt_allopen.UseVisualStyleBackColor = true;
+            this.bt_allopen.Click += new System.EventHandler(this.Bt_allopen_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(709, 219);
+            this.Controls.Add(this.bt_allopen);
+            this.Controls.Add(this.bt_delete);
+            this.Controls.Add(this.bt_open);
+            this.Controls.Add(this.bt_insert);
+            this.Controls.Add(this.lb_url);
+            this.Controls.Add(this.tb_url);
+            this.Name = "Form1";
+            this.Text = "URL Opener";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_url;
+        private System.Windows.Forms.ListBox lb_url;
+        private System.Windows.Forms.Button bt_insert;
+        private System.Windows.Forms.Button bt_open;
+        private System.Windows.Forms.Button bt_delete;
+        private System.Windows.Forms.Button bt_allopen;
+    }
+}
+
---
+++ url_open/url_open/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace url_open
+{
+    public partial class Form1 : Form
+    {
+        String url;
+
+        public Form1()
+        {
+            InitializeComponent();
+            bt_delete.Enabled = false;
+        }
+
+        private void Bt_insert_Click(object sender, EventArgs e)
+        {
+            if (tb_url.Text == "")
+            {
+                MessageBox.Show("URLを入力してください");
+            }
+            else
+            {
+                lb_url.Items.Add(tb_url.Text);
+            }
+        }
+
+        private void Bt_delete_Click(object sender, EventArgs e)
+        {
+            int sel = lb_url.SelectedIndex;
+            if (sel == -1) { }
+            else
+            {
+                lb_url.Items.RemoveAt(sel);
+            }
+        }
+
+        private void Lb_url_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lb_url.SelectedItems.Count > 0)
+            {
+                bt_delete.Enabled = true;
+            }
+            else
+            {
+                bt_delete.Enabled = false;
+            }
+        }
+
+        private void Bt_open_Click(object sender, EventArgs e)
+        {
+            if (lb_url.SelectedItems.Count > 0)
+            {
+                url = lb_url.SelectedItem.ToString();
+            }
+            //to Do
+            //urlが有効かどうかチェックする
+            try
+            {
+                System.Diagnostics.Process.Start(url);
+            }
+            catch (Exception) {
+                MessageBox.Show("正しいURLを選択してください");
+            }
+        }
+
+        private void Bt_allopen_Click(object sender, EventArgs e)
+        {
+            for(int i = 0; i < lb_url.Items.Count; i++)
+            {
+                url = lb_url.Items[i].ToString();
+                try
+                {
+                    System.Diagnostics.Process.Start(url);
+                }
+                catch (Exception) { }
+            }
+        }
+    }
+}
